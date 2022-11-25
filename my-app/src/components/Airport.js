826449b03f31fc7f616175a7b7bc8a8ee5023eec
Eva Liu <liuxiner2002@gmail.com>
2022-11-25 12:56:51 -0800

    setup

---
+++ my-app/src/components/Airport.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+const Airport = (props) => {
+    /**
+    * Get back-end response
+    * @param response
+    * @returns {Promise<never>|*}
+    */
+    const checkStatus = (response) => {
+        if (response.status >= 200 && response.status < 300 || response.status === 0) {
+            return response;
+        } else {
+            return Promise.reject(new Error(response.status + ": " + response.statusText));
+        }
+    };
+
+    const [long, setLong] = useState([]);
+    const [lat, setLat] = useState([]);
+    const [name, setName] = useState([]);
+}
