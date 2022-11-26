6cd2a3654911f1f401d910d1deae4858123e69bd
Eva Liu <liuxiner2002@gmail.com>
2022-11-25 21:51:34 -0800

    yay graph shows

--- my-app/src/components/Graph.js
+++ my-app/src/components/Graph.js
@@ -1,34 +1,38 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import { VegaLite } from 'react-vega'
 import { Handler } from 'vega-tooltip'
 
-const spec = {
-  width: 400,
-  height: 200,
-  mark: 'bar',
-  encoding: {
-    x: { field: 'a', type: 'ordinal' },
-    y: { field: 'b', type: 'quantitative' },
-  },
-  data: { name: 'table' }, // note: vega-lite data attribute is a plain object instead of an array
+const Graph = () => {
+
+  const spec = {
+    width: 400,
+    height: 200,
+    mark: 'bar',
+    encoding: {
+      x: { field: 'a', type: 'ordinal' },
+      y: { field: 'b', type: 'quantitative' },
+    },
+    data: { name: 'table' }, // note: vega-lite data attribute is a plain object instead of an array
+  }
+
+  const barData = {
+    table: [
+      { a: 'A', b: 28 },
+      { a: 'B', b: 55 },
+      { a: 'C', b: 43 },
+      { a: 'D', b: 91 },
+      { a: 'E', b: 81 },
+      { a: 'F', b: 53 },
+      { a: 'G', b: 19 },
+      { a: 'H', b: 87 },
+      { a: 'I', b: 52 },
+    ],
+  }
+
+  return (
+    <VegaLite spec={spec} data={barData} tooltip={new Handler().call} />
+  );
+
 }
 
-const barData = {
-  table: [
-    { a: 'A', b: 28 },
-    { a: 'B', b: 55 },
-    { a: 'C', b: 43 },
-    { a: 'D', b: 91 },
-    { a: 'E', b: 81 },
-    { a: 'F', b: 53 },
-    { a: 'G', b: 19 },
-    { a: 'H', b: 87 },
-    { a: 'I', b: 52 },
-  ],
-}
-
-ReactDOM.render(
-  <VegaLite spec={spec} data={barData} tooltip={new Handler().call} />,
-  document.getElementById('bar-container')
-);
+export default Graph;
