ecea7a8de9304c6016cf631a4644c63d039d9065
Tony Yuan <tonyyuan982@gmail.com>
2022-11-26 20:28:19 -0800

    basic intro screen done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
+import React, { useState } from 'react'
 import Graph from './components/Graph'
+import Intro from './components/Intro'
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
+      <div id="content">
+        <Intro /> 
+        <header className="App-header">
+          Where When You Fly V2
+        </header>
         <p>
           Graph
         </p>
         <Graph />
-      </header>
+      </div>
     </div>
   );
 }
