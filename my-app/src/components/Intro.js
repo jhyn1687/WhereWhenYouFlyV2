901fb4bdeed5824d59e3f870da207fb5ed1790f6
Tony Yuan <jhyn1687@uw.edu>
2022-11-26 18:40:00 -0800

    Started work on intro screen

---
+++ my-app/src/components/Intro.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { IoIosAirplane as PlaneIcon } from 'react-icons/IoIos';
+
+const Intro = () => {
+
+  return (
+    <>
+    <div id="intro-screen">
+      <h1>Where When You Fly V2 <PlaneIcon /></h1>
+      <h2>Controls:</h2>
+      <p>Use left mouse button </p>
+    </div>
+    </>
+  )
+}
+
+export default Intro;
